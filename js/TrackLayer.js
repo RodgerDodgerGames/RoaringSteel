4a7c9f2cfd91024ed8d23c511559ce4f4accf447
Kris Johnson <krisj@northpointgis.com>
2016-04-21 16:28:14 -0500

    towns as feature layer

--- js/TrackLayer.js
+++ js/TrackLayer.js
@@ -1,32 +1,10 @@
 // TrackLayer.js
 
-// This class extends L.MeasuringTool
+// This class calculates the cost of track being laid
 
-L.TrackLayer = L.Polyline.Measure.extend({
-    addHooks: function () {
-        L.Draw.Polyline.Measure.prototype.addHooks.call(this);
-        if (this._map) {
-            this._startLayingTrack();
-        }
-    },
+L.TrackLayer = L.Class.extend({
+    initialize: function (map, options) {
 
-    // get current distance
-    _startLayingTrack: function() {
-        this._map
-            .on('pointermove', this._onCursorMove, this)
-            .on('mousemove', this._onCursorMove, this);
-        // console.log('distance', this.__getMeasurementString());
-    },
-
-    _onCursorMove: function() {
-        var currentLatLng = this._currentLatLng,
-            previousLatLng = this._markers[this._markers.length - 1].getLatLng(),
-            distance,
-
-            // calculate the distance from the last fixed point to the mouse position
-            distance = currentLatLng.distanceTo(previousLatLng);
-
-        console.log('distance', distance);
     }
 
 
--- js/main.js
+++ js/main.js
@@ -1,30 +1,61 @@
-$(function(){
-    var Stamen_TerrainBackground = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/terrain-background/{z}/{x}/{y}.{ext}', {
-            attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-            subdomains: 'abcd',
-            minZoom: 4,
-            maxZoom: 18,
-            ext: 'png',
-            bounds: [[22, -132], [70, -56]]
-        }),
-        Stamen_TonerLabels = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/toner-labels/{z}/{x}/{y}.{ext}', {
-            attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-            subdomains: 'abcd',
-            minZoom: 0,
-            maxZoom: 20,
-            ext: 'png'
-        }),
+(function() {
+    $(function(){
+        var Stamen_TerrainBackground = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/terrain-background/{z}/{x}/{y}.{ext}', {
+                attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+                subdomains: 'abcd',
+                minZoom: 4,
+                maxZoom: 18,
+                ext: 'png',
+                bounds: [[22, -132], [70, -56]]
+            }),
+            Stamen_TonerLabels = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/toner-labels/{z}/{x}/{y}.{ext}', {
+                attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+                subdomains: 'abcd',
+                minZoom: 0,
+                maxZoom: 20,
+                ext: 'png'
+            }),
 
-        map = new L.Map('map', {
-            layers: [Stamen_TerrainBackground, Stamen_TonerLabels],
-            center: new L.LatLng(38.221863, -104.780391),
-            zoom: 12
-        });
-        // trackLayer = new L.TrackLayer(map);
+            map = new L.Map('map', {
+                layers: [Stamen_TerrainBackground, Stamen_TonerLabels],
+                center: new L.LatLng(39.073348, -108.572076),
+                zoom: 14
+            });
+            // trackLayer = new L.TrackLayer(map);
+            // load layers
+            loadLayers(map);
 
-        // add measure control
-        L.Control.measureControl().addTo(map);
+            // add measure control
+            L.Control.measureControl().addTo(map);
+
+    });
+    // end doc ready function
+
+    // load map layers
+    function loadLayers(map) {
+        // towns layer dynamic
+        // L.esri.dynamicMapLayer({
+        //   url: "http://services.nationalmap.gov/arcgis/rest/services/geonames/MapServer",
+        //   layers: [4],
+        //   opacity: 1
+        // }).addTo(map);
+        // towns feature layer
+        L.esri.featureLayer({
+            url: 'http://services.nationalmap.gov/arcgis/rest/services/geonames/MapServer/4',
+            pointToLayer: function (geojson, latlng) {
+              return L.marker(latlng, {
+                icon: L.icon({
+                  iconUrl: 'images/town.png',
+                  iconSize: [36, 36],
+                  iconAnchor: [18, 18],
+                  // popupAnchor: [0, -11],
+                })
+              });
+            },
+          }).addTo(map);
 
 
-});
+    }
+
+})();
 

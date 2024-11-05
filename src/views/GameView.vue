32d40d1a18d6d66fab5ec5448b4fad85b41d2c81
Kris Johnson <rumski20@msn.com>
2024-11-04 21:13:31 -0600

    remove container class

--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -11,31 +11,29 @@ const gameStore = useGameStore()
 </script>
 
 <template>
-  <div class="container is-fluid">
-    <div class="columns">
-      <!-- Main Map -->
-      <div class="column is-three-quarters">
-        <MainMap />
-      </div>
+  <div class="columns">
+    <!-- Main Map -->
+    <div class="column is-three-quarters">
+      <MainMap />
+    </div>
 
-      <!-- Side Panel -->
-      <div class="column is-one-quarter">
-        <div class="panel">
-          <div class="panel-heading">
-            <p class="panel-title">Side Panel</p>
-          </div>
-          <div class="panel-tabs">
-            <a class="panel-tab is-active">Tab 1</a>
-            <a class="panel-tab">Tab 2</a>
-          </div>
-
-          <!-- show draw buttons when map is available -->
-          <DrawButtons
-            v-if="gameStore.townsLayer"
-            :map="gameStore.mainMap"
-            :towns="gameStore.townsLayer"
-          />
+    <!-- Side Panel -->
+    <div class="column is-one-quarter">
+      <div class="panel">
+        <div class="panel-heading">
+          <p class="panel-title">Side Panel</p>
         </div>
+        <div class="panel-tabs">
+          <a class="panel-tab is-active">Tab 1</a>
+          <a class="panel-tab">Tab 2</a>
+        </div>
+
+        <!-- show draw buttons when map is available -->
+        <DrawButtons
+          v-if="gameStore.townsLayer"
+          :map="gameStore.mainMap"
+          :towns="gameStore.townsLayer"
+        />
       </div>
     </div>
   </div>

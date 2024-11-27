229b13425d6528ff00a8cd0b751ff74aef2f3da7
Kris Johnson <rumski20@msn.com>
2024-11-27 00:55:03 -0600

    add welcome view

--- src/views/WelcomeView.vue
+++ src/views/WelcomeView.vue
@@ -1,27 +1,40 @@
 <template>
-  <section class="hero is-primary is-fullheight">
+  <section class="hero is-fullheight">
     <div class="hero-body">
-      <div class="container has-text-centered">
-        <h1 class="title">Welcome to Roaring Steel</h1>
-        <h2 class="subtitle">Your adventure begins here</h2>
-        <button class="button is-large is-info mt-5" @click="startGame">Start Game</button>
+      <div id="welcomeContainer" class="container has-text-centered">
+        <h1 class="title roaring-title">Welcome to Roaring Steel</h1>
+        <h2 class="subtitle is-3">Your adventure begins here</h2>
+        <div class="buttons are-large mt-6 is-flex is-justify-content-center">
+          <button class="button is-info is-outlined mr-6" @click="newGame">New Game</button>
+          <button class="button is-link is-outlined" @click="loadGame">Load Game</button>
+        </div>
       </div>
     </div>
   </section>
 </template>
 
 <script setup>
-const startGame = () => {
-  // Logic to transition to the next screen
-  console.log('Game started')
+function newGame() {
+  console.log('new game clicked')
+}
+
+function loadGame() {
+  console.log('load game clicked')
 }
 </script>
 
 <style scoped>
 /* Additional styling for the welcome screen */
 .hero {
-  background-image: url('/path/to/your/background-image.jpg');
+  background-image: url('src/assets/images/Unknown.jpg');
   background-size: cover;
   background-position: center;
+}
+.roaring-title {
+  font-family: 'Saddlebag';
+  font-size: 4rem;
+}
+#welcomeContainer {
+  margin-bottom: 30%;
 }
 </style>
